b1374146ffd3296dacdd56964f8ead2724c78f7e
Matthew Fernandez <MatthewAFernandez@Matthew-Fernandezs-iMac.local>
2017-01-27 14:11:48 -0800

    fixed tabbing

--- client/rounds/golfer.js
+++ client/rounds/golfer.js
@@ -2,18 +2,17 @@ angular.module('golfCompanion.messages', [])
   .controller('golferController', ($scope, Scores) => {
     $scope.scores = [];
 
-    $scope.getScores = () => {
-      Scores.getScores().then(score => {
-        $scope.scores = score;
-      });
-    }
-    $scope.getScores();
+  $scope.getScores = () => {
+    Scores.getScores().then(score => {
+      $scope.scores = score;
+    });
+  }
+  $scope.getScores();
 
-    $scope.postScore = (val) => {
-      Scores.postScore(val).then(score => {
-        $scope.posted = score;
-        $scope.getScores();
-      });
-    };
-
-  });
+  $scope.postScore = (val) => {
+    Scores.postScore(val).then(score => {
+      $scope.posted = score;
+      $scope.getScores();
+    });
+  };
+});
