851408862972eaf54ee494cf878dd5b0925c007c
Matthew Fernandez <MatthewAFernandez@Matthew-Fernandezs-iMac.local>
2017-01-27 14:19:26 -0800

    skeleton post

--- client/services/services.js
+++ client/services/services.js
@@ -1,6 +1,18 @@
 angular.module('golfCompanion.services', [])
   .factory('Scores', ($http) => {
     return {
+
+      postScore: (val) => {
+        let data = {val: val};
+        return $http({
+          method: 'POST',
+          url: '/golfer',
+          data: data
+        })
+        .then(res => {
+          return res.data;
+        })
+      },
       getScores: () => {
         return $http({
           method: 'GET',
