325a9445a671c9e64f56d8dadc93c7c695aa3154
Matthew Fernandez <MatthewAFernandez@Matthew-Fernandezs-iMac.local>
2017-01-30 11:17:17 -0800

    install and require highcharts

--- server/index.js
+++ server/index.js
@@ -4,15 +4,20 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const _ = require('lodash');
+const highcharts = require('highcharts');
 
 const app = express();
 
+// parsing into JSON
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 
+// serve the client the static client folder
 app.use(express.static(__dirname + '/../client'));
 
+
+// connect to the mongoose schema
 mongoose.connect('mongodb://localhost/golfCompanion');
 mongoose.connection.once('open', (req, res, next) => {
 
