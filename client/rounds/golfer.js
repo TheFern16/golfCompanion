5ad4c36455349e0c6a28bc6e87616421bd87bcb3
Matthew Fernandez <MatthewAFernandez@Matthew-Fernandezs-iMac.local>
2017-01-30 10:13:34 -0800

    invoking but is not re rendering the page

--- client/rounds/golfer.js
+++ client/rounds/golfer.js
@@ -8,30 +8,13 @@ angular.module('golfCompanion.scores', ['golfCompanion.services'])
       $scope.data.scores = scores;
     });
   }
-  $scope.getScores();
+  console.log('invoked -----', $scope.getScores());
 
   $scope.postScore = (score) => {
-    console.log('here');
     Scores.postScore(score).then((data) => {
-      $scope.feedback = data;
-      $scope.getScores();
-    });
-  };
+      $scope.postedScore = data;
+    })
+    console.log('invoked ======', $scope.getScores());
+  }
 
 });
-
-
-// <script>
-// angular.module('submitExample', [])
-//   .controller('ExampleController', ['$scope', function($scope) {
-//     $scope.list = [];
-//     $scope.text = 'hello';
-//     $scope.submit = function() {
-//       if ($scope.text) {
-//         $scope.list.push(this.text);
-//         $scope.text = '';
-//       }
-//     };
-//   }]);
-// </script>
-
