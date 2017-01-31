f278d4e9ca36d437702790e8f2a8a823a46aa298
Matthew Fernandez <MatthewAFernandez@Matthew-Fernandezs-iMac.local>
2017-01-31 09:09:20 -0800

    refactored and added the graph to the angular controller

--- client/rounds/golfer.js
+++ client/rounds/golfer.js
@@ -4,20 +4,42 @@ angular.module('golfCompanion.scores', ['golfCompanion.services'])
     $scope.name = 'Joe Golfer';
     $scope.score = 72;
     $scope.course = 'Rustic Canyon G.C.';
+    $scope.scores = [85, 88, 85, 83, 78, 79, 75, 80, 73, 74, 72]
 
   $scope.getScores = () => {
     Scores.getScores().then(scores => {
       $scope.data.scores = scores;
     });
   }
-  console.log('invoked -----', $scope.getScores());
 
   $scope.postScore = (name, course, score) => {
     Scores.postScore(name, course, score).then((data) => {
       $scope.inputConfirmed = "--- Your score has been posted."
       $scope.result = data;
-    console.log('invoked ======', $scope.getScores());
     })
   }
 
+  $scope.analysis =
+  $(function () {
+    const golferData = Highcharts.chart('container', {
+      chart: {
+        type: 'areaspline'
+      },
+      title: {
+        text: 'Your Stats'
+      },
+      xAxis: {
+        categories: ['Janurary']
+      },
+      yAxis: {
+        title: {
+          text: 'Your Scores'
+        }
+      },
+      series: [{
+        name: 'Joe Golfer',
+        data: $scope.scores
+      }]
+    });
+  })
 });
