abe5a96073318be063adafe3ad944868476ce5d7
Matthew Fernandez <MatthewAFernandez@Matthew-Fernandezs-iMac.local>
2017-02-01 09:32:06 -0800

    working with jquery

--- client/rounds/golfer.js
+++ client/rounds/golfer.js
@@ -21,68 +21,74 @@ angular.module('golfCompanion.scores', ['golfCompanion.services', 'underscore'])
       });
     };
 
+    $scope.analysis = $(document).ready(function() {
 
-    $scope.analysis = $(() => {
-      const golferData = Highcharts.chart('container', {
+      var options = {
         chart: {
-          type: 'areaspline'
+            renderTo: 'container',
+            type: 'spline'
         },
-        events: {
-          load: function () {
-            setInterval(() => {
-              console.log(this.series[0].data())
-            }, 1000);
-          }
-        },
-        title: {
-          text: 'Your Stats'
-        },
-        xAxis: {
-          categories: ['Janurary']
-        },
-        yAxis: {
-          title: {
-            text: 'Your Scores'
-          }
-        },
-        series: [{
-          name: 'Joe Golfer',
-          data: (function() {
-            let data = [];
-            Scores.getScores().then(scores => {
-              // console.log(Array.isArray(scores))
-              // console.log('scores', scores)
-              scores.map((element) => {
-                console.log(element)
-                return element.score
-              })
-              _.each(scores, (element) => {
-                data.push(element.score);
-              });
-            });
-            console.log(JSON.parse(JSON.stringify(data)));
-            return data;
-          }())
-        }]
-      });
+        series: [{}]
+      };
 
-      $('#small').click(function () {
-        chart.setSize(400);
+      $.getJSON('data.json', function(data) {
+          options.series[0].data = data;
+          var chart = new Highcharts.Chart(options);
       });
-
-      $('#large').click(function () {
-        chart.setSize(800);
-      });
-
-      $('#auto').click(function () {
-        chart.setSize(null);
-      });
-
     });
-  });
+
+    $('#small').click(function () {
+      chart.setSize(400);
+    });
+
+    $('#large').click(function () {
+      chart.setSize(800);
+    });
+
+    $('#auto').click(function () {
+      chart.setSize(null);
+    });
+})
 
 
 
 
 
 
+// $scope.analysis = $(document).ready(() => {
+//     const golferData = Highcharts.chart('container', {
+//       chart: {
+//         type: 'areaspline'
+//       },
+//       title: {
+//         text: 'Your Stats'
+//       },
+//       xAxis: {
+//         categories: ['Janurary']
+//       },
+//       yAxis: {
+//         title: {
+//           text: 'Your Scores'
+//         }
+//       },
+//       series: [{
+//         name: 'Joe Golfer',
+//         data: (function() {
+//           let data = [];
+//           Scores.getScores().then(scores => {
+//             // console.log(Array.isArray(scores))
+//             // console.log('scores', scores)
+//             scores.map((element) => {
+//               console.log(element)
+//               return element.score
+//             })
+//             _.each(scores, (element) => {
+//               data.push(element.score);
+//             });
+//           });
+//           console.log(JSON.parse(JSON.stringify(data)));
+//           return data;
+//         }())
+//       }]
+//     });
+//   });
