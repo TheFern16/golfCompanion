b580734d6475cb7c46e936c84586f48161e0f710
poplockndropit <matthew.allen.fernandez@gmail.com>
2017-02-02 11:34:47 -0800

    removed weather controller because I might need a weather model for the api call

--- server/controllers/GolferController.js
+++ server/controllers/GolferController.js
@@ -3,11 +3,12 @@ const restful = require('node-restful');
 module.exports = (app, route) => {
 
   let rest = restful.model('golfer', app.models.golfer)
-  .methods(['get', 'put', 'post', 'delete'])
+    .methods(['get', 'put', 'post', 'delete'])
 
   rest.register(app, route);
 
   return (req, res, next) => {
     next();
   };
+
 };
