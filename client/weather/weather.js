771e5e421e814fc585959b9af7abef186b7abe7e
poplockndropit <matthew.allen.fernandez@gmail.com>
2017-02-02 12:19:30 -0800

    add html 5 geolocation feature

--- client/weather/weather.js
+++ client/weather/weather.js
@@ -1,11 +1,20 @@
 angular.module('golfCompanion.weather', [])
   .factory('Weather', function($http) {
+    $scope.location = '';
+    $scope.initial = function(){
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
 
-    let getTheWeather = () => {
-      $.get('https://www.amdoren.com/api/weather.php?api_key=IBZzdLmM2yCYaXjgTZ6x&lat=40.7127837&lon=-74.0059413', {
-
-      })
-    }
-
-
+      $http.jsonp("https://www.amdoren.com/api/weather.php?api_key=IBZzdLmM2yCYaXjgTZ6x&lat="+lat+"&lon="+lon+"&callback=JSON_CALLBACK").
+        success((data) => {
+          $scope.weatherData = data;
+          $('.loading').hide();
+        })
+        .error(() => {
+          $('.loading').hide();
+          $('.error').show().html("Sorry there has been an error connecting to the API");
+        });
+      });
+    };
   })
