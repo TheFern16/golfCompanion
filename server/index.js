d07bea4bc9f386240193117f5722471ff7627c67
poplockndropit <matthew.allen.fernandez@gmail.com>
2017-02-02 16:45:02 -0800

    fxd cors formatting issues

--- server/index.js
+++ server/index.js
@@ -17,8 +17,8 @@ app.use(express.static(__dirname + '/../client'));
 
 // allow CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
 
