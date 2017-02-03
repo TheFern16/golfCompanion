dc2876ff092e907679846f28a4275d1f56ef96ae
poplockndropit <matthew.allen.fernandez@gmail.com>
2017-02-02 21:05:04 -0800

    displays weather data on both pages

--- client/stats/stats.js
+++ client/stats/stats.js
@@ -1,6 +1,6 @@
-angular.module('golfCompanion.stats', [])
+angular.module('golfCompanion.stats', ['golfCompanion.scores'])
 
-.controller('StatsController', function($scope) {
+.controller('StatsController', function($scope, $http) {
 
   $scope.areaspline = $(document).ready(() => {
     let options = {
@@ -30,4 +30,17 @@ angular.module('golfCompanion.stats', [])
     });
   });
 
+  $scope.getWeather = () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        let url = "https://cors-anywhere.herokuapp.com/http://weathers.co/api.php?city=New+York"
+      $http.get(url)
+        .then((data) => {
+          $scope.weatherData = data;
+          console.log($scope.weatherData);
+        });
+      });
+    };
+  $scope.getWeather();
 });
