cafb9bc14d01d1f341ef974f04ce7a8baba9701c
poplockndropit <matthew.allen.fernandez@gmail.com>
2017-02-02 19:27:14 -0800

    added express cors middlewhere and began first implementation

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const routes = require('./routes');
+const cors = require('cors');
 const _ = require('lodash');
+const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors())
 
 mongoose.connect('mongodb://localhost/golfCompanion');
 
@@ -18,23 +21,31 @@ app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(express.static(__dirname + '/../client'));
 
 
-// allow CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-})
+// CORS support
+const whiteList = ['https://www.amdoren.com/api/time.php'];
 
-// connect to the mongoose schema
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  if (whiteList.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }
+  } else {
+    corsOptions = { origin: false }
+  }
+  callback(null, corsOptions);
+}
 
+
+
+// serve up the routes
 app.models = require('./models/index');
 
 _.each(routes, (controller, route) => {
   app.use(route, controller(app, route));
 });
 
-app.listen(3000, () => {
+
+// are you listening?
+app.listen(port, () => {
   console.log('3000... I am listening');
 });
 
